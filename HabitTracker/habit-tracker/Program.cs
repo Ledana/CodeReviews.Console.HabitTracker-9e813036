55c0ab849235b1d3b177c06955812925332a563d
Ledana <ledanagjoka66@gmail.com>
2026-04-07 15:14:12 +0200

    Added my habit tracker project

---
+++ HabitTracker/habit-tracker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using habit_tracker;
+using Microsoft.Data.Sqlite;
+
+public class Program
+{
+    static string connectionString = @"Data Source=habit-Tracker.db";
+    public static void Main(string[] args)
+    {
+        using var connection = new SqliteConnection(connectionString);
+        connection.Open();
+        var tableCmd = connection.CreateCommand();
+
+        tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS habits (
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Name TEXT NOT NULL,
+        Date TEXT,
+        Quantity INTEGER,
+        Unit Text
+        )";
+
+        tableCmd.ExecuteNonQuery();
+
+        connection.Close();
+
+        Tracker.GetUserInput();
+    }
+    
+}
+public class Habit
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public int Quantity { get; set; }
+    public string Unit { get; set; }
+}
+public class DrinkingWater
+{
+    public int Id {get; set;}
+    public DateTime Date {get; set;}
+    public int Quantity {get; set;}
+}
